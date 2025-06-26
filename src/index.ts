31681d686b66e6f09965896bb5b1bc7c6c3108c0
Søren Anton Smidt <au754363@uni.au.dk>
2025-06-26 14:08:45 +0200

    Implemented dynamic displaytext for the restStopSlider input

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
 console.log("Hello from index file");
+
+const restStopSlider: HTMLInputElement = document.querySelector("#restStopTime")!; 
+const restStopSliderTimerText: HTMLDivElement = document.querySelector("#restStopTimeDisplayText")!;
+
+restStopSlider.addEventListener("input", () => {
+    const timeValue: number = parseInt(restStopSlider.value);
+    restStopSliderTimerText.innerHTML = formatTime(timeValue);  
+});
+
+function formatTime(timeValue: number): string {
+    const minutes: string = Math.floor(timeValue / 60).toString().padStart(2, "0");
+    const seconds: string = (timeValue % 60).toString().padStart(2, "0");
+    return `${minutes}m:${seconds}s`;
+}
