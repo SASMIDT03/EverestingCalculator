2c9fe7bd7fa6cb64b6d55a3fe75f5ea7197669a6
Søren Anton Smidt <au754363@uni.au.dk>
2025-06-29 22:08:09 +0200

    Implement StandardCalculator methods and corresponding tests

--- src/StandardCalculator.ts
+++ src/StandardCalculator.ts
@@ -1,31 +1,56 @@
 import { Calculator } from "./framework/Calculator";
 
 export class StandardCalculator implements Calculator {
-
+    private lapDistance: number;
+    private climbingTime: number;
+    private totalNumberOfLaps: number;
+    private totalDistance: number;
+    
     constructor() {
-        
+        this.lapDistance = 0;
+        this.climbingTime = 0;
+        this.totalNumberOfLaps = 0;
+        this.totalDistance = 0;
     }
 
     getLapDistance(): number {
-        throw new Error("Method not implemented.");
+        return this.lapDistance;
     }
     getClimbingTime(): number {
-        throw new Error("Method not implemented.");
+        return this.climbingTime;
     }
     getTotalNumberOfLaps(): number {
-        throw new Error("Method not implemented.");
+        return this.totalNumberOfLaps;
     }
     getTotalDistance(): number {
-        throw new Error("Method not implemented.");
+        return this.totalDistance;
     }
+
+    updateData(climbingDistance: number, elevationGain: number, climbingSpeed: number, totalWeight: number): void {
+        this.lapDistance = 2 * climbingDistance;
+        this.climbingTime = climbingDistance / climbingSpeed * 60 * 60;
+        this.totalNumberOfLaps = Math.round(8849 / elevationGain);
+        this.totalDistance = parseFloat((this.totalNumberOfLaps * this.lapDistance).toFixed(2));
+
+        // update watts
+    }
+
     calculateClimbingWatts(totalWeight: number, climbHeight: number, climbingTime: number): number {
-        throw new Error("Method not implemented.");
+        return (totalWeight * 9.82 * climbHeight) / climbingTime;
     }
+
     calculateAirResistanceWatts(climbingSpeed: number): number {
-        throw new Error("Method not implemented.");
+        const airDensity = 1.225;
+        const cda = 0.5;
+        const frontalArea = 0.5;
+
+        return (1/2) * airDensity * cda * frontalArea * (climbingSpeed / 3.6)**3;
     }
+
     calculateRollingResistanceWatts(weight: number, climbingSpeed: number): number {
-        throw new Error("Method not implemented.");
+        const rollingCoefficient = 0.004
+
+        return weight * 9.82 * rollingCoefficient * (climbingSpeed / 3.6)
     }
 
 }
--- test/Calculator.test.ts
+++ test/Calculator.test.ts
@@ -8,6 +8,50 @@ beforeEach(() => {
   calculator = new StandardCalculator();
 });
 
-test("TestTEst", () => {
-    expect(3).toBe(3);
+test("Should return lapDistance 0 for empty calculator", () => {
+    expect(calculator.getLapDistance()).toBe(0);
+})
+
+test("Should return climbingTime 0 for empty calculator", () => {
+  expect(calculator.getClimbingTime()).toBe(0);
+})
+
+test("Should return total number of laps 0 for empty calculator", () => {
+  expect(calculator.getTotalNumberOfLaps()).toBe(0);
+})
+
+test("Should return totalDistance 0 for empty calculator", () => {
+  expect(calculator.getTotalDistance()).toBe(0);
+})
+
+test("Should have lapDistance 4.1 when climbDistance updated to 2.05", () => {
+  calculator.updateData(2.05, 0, 0, 0);
+  expect(calculator.getLapDistance()).toBe(4.1);
+})
+
+test("Should have climbingTime x for climbDistance 2.05, climbingSpeed 14.5", () => {
+  calculator.updateData(2.05, 0, 14.5, 0);
+  expect(calculator.getClimbingTime()).toBe(508.9655172413792);
+})
+
+test("Should have totalNumberOfLaps 101 for climbDistance 2.05, elevationGain 88", () => {
+  calculator.updateData(2.05, 88, 0, 0);
+  expect(calculator.getTotalNumberOfLaps()).toBe(101);
+})
+
+test("Should have totalDistance 404 for climbDistance 2.05, elevationGain 88", () => {
+  calculator.updateData(2.05, 88, 0, 0);
+  expect(calculator.getTotalDistance()).toBe(414.1);
+})
+
+test("Should have gravityWatts 120 for totalWeight 71, elevationGain 88, climbingTime 508", () => {
+  expect(calculator.calculateClimbingWatts(71, 88, 508.9655172413792)).toBe(120.54914905149055);
+})
+
+test("Should have airRestitancyWatts 10 for climbingSpeed 14.5", () => {
+  expect(calculator.calculateAirResistanceWatts(14.5)).toBe(10.005587772740911);
+})
+
+test("Should have rollingResistancy 11 for totalWeight 71, climbingSpeed 14.5", () => {
+  expect(calculator.calculateRollingResistanceWatts(71, 14.5)).toBe(11.23298888888889);
 })
