cafa2dc8be34903470d3867a2f323e9072ae683e
Søren Anton Smidt <au754363@uni.au.dk>
2025-06-30 09:17:04 +0200

    Added Enum for calculation constants

---
+++ src/CalculationConstants.ts
@@ -0,0 +1,9 @@
+export enum CalculationConstants {
+    HeightOfMountEverest = 8849,
+    LocalGravitationalConstant = 9.82, // Local for Denmark
+    AirDensity = 1.22601, // Approx at 15 celcius
+    CDA = 0.63,
+    FrontalArea = 0.5,
+    RollingCoefficient = 0.005,
+    DriveTrainLoss = 0.03
+}
