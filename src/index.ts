1b253d04c19943473fb47f69f49bf62003f7bf03
Søren Anton Smidt <au754363@uni.au.dk>
2025-06-30 11:00:24 +0200

    Started connecting the StandardCalculator

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
+import { Calculator } from "./framework/Calculator";
+import { StandardCalculator } from "./StandardCalculator.js";
+
+const calculator: Calculator = new StandardCalculator();
+
 console.log("Hello from index file");
+
+const calculateButton: HTMLButtonElement = document.querySelector("#calculateDataButton")!;
+
+const distanceOfClimbInput: HTMLInputElement = document.querySelector("#climbingDistance")!;
+const elevationGainInput: HTMLInputElement = document.querySelector("#elevation")!;
+const totalWeightInput: HTMLInputElement = document.querySelector("#totalWeight")!;
+const climbingSpeedInput: HTMLInputElement = document.querySelector("#climbingSpeed")!;
+const descendingSpeedInput: HTMLInputElement = document.querySelector("#descentSpeed")!;
 
 const restStopSlider: HTMLInputElement = document.querySelector("#restStopTime")!; 
 const restStopSliderTimerText: HTMLDivElement = document.querySelector("#restStopTimeDisplayText")!;
+
+const totalNumberOfRestStopInput: HTMLInputElement = document.querySelector("#totalRestStops")!;
 
 restStopSlider.addEventListener("input", () => {
     const timeValue: number = parseInt(restStopSlider.value);
@@ -13,3 +28,18 @@ function formatTime(timeValue: number): string {
     const seconds: string = (timeValue % 60).toString().padStart(2, "0");
     return `${minutes}m:${seconds}s`;
 }
+
+calculateButton.addEventListener("click", () => {
+
+    // TODO: Guard check
+
+    let distanceOfClimb: number = parseFloat(distanceOfClimbInput.value);
+    let elevationGain: number = parseFloat(elevationGainInput.value);
+    let totalWeight: number = parseFloat(totalWeightInput.value);
+    let climbingSpeed: number = parseFloat(climbingSpeedInput.value);
+    let descendingSpeed: number = parseFloat(descendingSpeedInput.value);
+    let restStopTime: number = parseFloat(restStopSlider.value);
+    let numberOfRestStops: number = parseFloat(totalNumberOfRestStopInput.value);
+
+    calculator.updateData(distanceOfClimb, elevationGain, climbingSpeed, totalWeight);
+})
